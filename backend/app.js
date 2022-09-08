a22001394dd292404d1f4b44a883dc802bda0dfc
Grasso Lilian <lilian.grasso.pro@gmail.com>
2022-09-08 17:15:38 +0200

    :card_file_box: Connexion à la db avec mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
 
-app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
-});
+app.use(express.json());
+
+//Connexion DB
+mongoose.connect('mongodb://localhost:27017',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+//CORS
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+   next();
+ });
 
 module.exports = app;
